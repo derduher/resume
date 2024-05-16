752da92dc23d1e5f4766361323923343d022562c
derduher <patrick.weygand@gmail.com>
2024-05-15 20:16:30 -0700

    update to react-rc19

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import fs from "fs";
 import path from "path";
 import ReactDOMServer from "react-dom/server";
-import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 import App from "../src/App";
-
-const sheet = new ServerStyleSheet();
 
 const indexFile = path.resolve(__dirname, "../public/index.html");
 fs.readFile(indexFile, "utf8", (err, data) => {
@@ -15,21 +12,13 @@ fs.readFile(indexFile, "utf8", (err, data) => {
   try {
     const markup = ReactDOMServer.renderToStaticMarkup(
       <React.StrictMode>
-        <StyleSheetManager sheet={sheet.instance}>
-          <App />
-        </StyleSheetManager>
+        <App />
       </React.StrictMode>
     );
-    const styleTags = sheet.getStyleTags();
     process.stdout.write(
-      data.replace(
-        '<div id="root"></div>',
-        `<div id="root">${styleTags}${markup}</div>`
-      )
+      data.replace('<div id="root"></div>', `<div id="root">${markup}</div>`)
     );
   } catch (e) {
     console.error(e);
-  } finally {
-    sheet.seal();
   }
 });
